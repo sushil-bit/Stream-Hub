394be06c7ddd963cb5abf4a398c20cac372ab4ff
sushil-bit <sbsushil62@gmail.com>
2026-09-14 05:33:09 +0000

    fix

--- stream-hub/services/api.js
+++ stream-hub/services/api.js
@@ -23,4 +23,3 @@ export const fetchTrendingMovies = async () => {
                                                             return [];
                                                               }
                                                               };
-                                                              it
