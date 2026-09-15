728e1a4302d5d1e6fe330cc4f1c8e3e6fbdfe210
sushil-bit <sbsushil62@gmail.com>
2026-09-16 02:09:33 +0530

    Refactor navigation to include bottom tabs and search

--- stream-hub/App.js
+++ stream-hub/App.js
@@ -1,27 +1,39 @@
-// App.js
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import HomeScreen from "./src/screens/HomeScreen";
-import DetailsScreen from "./src/screens/DetailsScreen";
-import PlayerScreen from "./src/screens/PlayerScreen";
+import FloatingTabBar from './src/components/Navigation/FloatingTabBar';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import PlayerScreen from './src/screens/PlayerScreen';
+import SearchScreen from './src/screens/SearchScreen';
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator
+      tabBar={(props) => <FloatingTabBar {...props} />}
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Explore" component={SearchScreen} />
+      <Tab.Screen name="Details" component={DetailsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-      <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="Home" component={HomeScreen} />
-                            <Stack.Screen name="Details" component={DetailsScreen} />
-                                    <Stack.Screen
-                                              name="Player"
-                                                        component={PlayerScreen}
-                                                                  options={{ presentation: "fullScreenModal" }}
-                                                                          />
-                                                                                </Stack.Navigator>
-                                                                                    </NavigationContainer>
-                                                                                      );
-                                                                                      }
-                                                                                      
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Main" component={BottomTabs} />
+        <Stack.Screen name="PlayerScreen" component={PlayerScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+        <Stack.Screen name="SearchScreen" component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
