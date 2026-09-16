8685c4015b40d6ed61a6711d8f8324d925acd0b8
sushil-bit <sbsushil62@gmail.com>
2026-09-16 22:42:03 +0000

    fix: remove duplicate AsyncStorage import in App component

--- stream-hub/App.js
+++ stream-hub/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { View, ActivityIndicator } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import AuthScreen from "./src/screens/AuthScreen";
 import { Ionicons } from '@expo/vector-icons';
 import DownloadsScreen from './src/screens/DownloadsScreen';
